7f73c7286f97141b63f31603ebdc8e9238901aaa
tahapaksu <taha@nox.ist>
2021-04-07 23:55:37 +0000

    styling

--- views/container.blade.php
+++ views/container.blade.php
@@ -1,9 +1,9 @@
 @extends($extends)
 @section("content")
-<div class="p-8 bg-white border-t">
+<div class="p-8">
     <div class="items-end w-full">
         <h2 class="mb-3 text-3xl font-semibold" style="line-height: 1em">@lang("Currency Management")</h2>
-        <p class="mb-4 text-sm font-light leading-5 text-gray-600">@lang("This page contains the available currencies, their rates and the configuration options that you can enable a site-wide currency or disable it.")</p>
+        <p class="mb-8 text-sm font-light leading-5 text-gray-600">@lang("This page contains the available currencies, their rates and the configuration options that you can enable a site-wide currency or disable it.")</p>
         @livewire("paksuco-currency::currencies")
     </div>
 </div>
